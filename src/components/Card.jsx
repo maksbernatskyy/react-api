f4dfd2798b2baa94fa61cefb4c6e1e58cec13853
maksbernatskyy <maksbernatskyy@gmail.com>
2025-11-12 14:19:59 +0100

    add component card

---
+++ src/components/Card.jsx
@@ -0,0 +1,27 @@
+export default function Card({image, name, birth_year, nationality, biography, awards}) {
+
+
+    return (
+        <>
+        <div className="col">
+            <div className="card p-0 h-100 shadow">
+                <img src={image} className="card-img-top" alt="" />
+                <div className="card-body">
+                    <h5 className="card-title">{name}</h5>
+                    <span className="d-block"><strong>Birth year:</strong> "{birth_year}";</span>
+                    <span className="d-block"><strong>Nationality:</strong> "{nationality}";</span>
+                    <p className="card-text"><strong>Biography:</strong> {biography}</p>
+                    <h6 className="fw-bold">Awards:</h6>
+                    <ul className="list-unstyled mb-0">
+                        {
+                            awards.map((thisAward, i) => (
+                                <li key={i}>{thisAward}</li>
+                            ))
+                        }
+                    </ul>
+                </div>
+            </div>
+        </div>
+        </>
+    )
+}
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,6 +1,9 @@
 import { useEffect, useState } from "react"
 import axios from "axios"
 
+{/* Import the card */}
+import Card from "./Card"
+
 export default function Main() {
 
     {/* State for Axios */}
@@ -19,28 +22,23 @@ export default function Main() {
         <>
         <main>
             <div className="container">
+                <div className="my-3">
+                    <h2 className="text-uppercase">actors:</h2>
+                </div>
+
+                {/* Grid Actors */}
                 <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 g-4">
                     {
                         todos.map((thisTodo) => (
-                            <div key={thisTodo.id} className="col">
-                                <div className="card p-0 h-100 shadow">
-                                    <img src={thisTodo.image} className="card-img-top" alt="" />
-                                    <div className="card-body">
-                                        <h5 className="card-title">{thisTodo.name}</h5>
-                                        <span className="d-block"><strong>Birth year:</strong> "{thisTodo.birth_year}";</span>
-                                        <span className="d-block"><strong>Nationality:</strong> "{thisTodo.nationality}";</span>
-                                        <p className="card-text"><strong>Biography:</strong> {thisTodo.biography}</p>
-                                        <h6 className="fw-bold">Awards:</h6>
-                                        <ul className="list-unstyled mb-0">
-                                            {
-                                                thisTodo.awards.map((thisAward, i) => (
-                                                    <li key={i}>{thisAward}</li>
-                                                ))
-                                            }
-                                        </ul>
-                                    </div>
-                                </div>
-                            </div>
+                            <Card 
+                              key={thisTodo.id}
+                              image={thisTodo.image}
+                              name={thisTodo.name}
+                              birth_year={thisTodo.birth_year}
+                              nationality={thisTodo.nationality}
+                              biography={thisTodo.biography}
+                              awards={thisTodo.awards}
+                            />
                         ))
                     }
                 </div>
