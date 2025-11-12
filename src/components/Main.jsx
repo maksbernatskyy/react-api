63370c0f48fa5275a84fb689bfbc8248ed646286
maksbernatskyy <maksbernatskyy@gmail.com>
2025-11-12 12:15:34 +0100

    add state for Axios

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,9 +1,15 @@
+import { useState } from "react"
+import axios from "axios"
+
 export default function Main() {
+
+    {/* State of Axios */}
+    const [todos, setTodos] = useState([])
 
     return (
         <>
         <main>
-            
+
         </main>
         </>
     )
