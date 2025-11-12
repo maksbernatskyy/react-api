6ac21a0bfcce67e6449569d08adb7798d8bc4277
maksbernatskyy <maksbernatskyy@gmail.com>
2025-11-12 12:42:08 +0100

    add list of names with axios

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import axios from "axios"
 
 export default function Main() {
@@ -6,10 +6,29 @@ export default function Main() {
     {/* State for Axios */}
     const [todos, setTodos] = useState([])
 
+    {/* Function call API */}
+    function fetchTodos() {
+        axios.get("https://lanciweb.github.io/demo/api/actors/")
+            .then((res) => setTodos(res.data))
+            .catch((err) => console.error(err))
+    }
+
+    useEffect(fetchTodos, [] )
+
     return (
         <>
         <main>
-
+            <div className="container">
+                <div className="card">
+                    <ul>
+                        {
+                            todos.map((thisTodo, i) => (
+                                <li key={i}>{thisTodo.name}</li>
+                            ))
+                        }
+                    </ul>
+                </div>
+            </div>
         </main>
         </>
     )
